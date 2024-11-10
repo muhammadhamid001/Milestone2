569dc70c88dd04fb3f288f77aaf12802957887a8
Muhammad Hamid <hamidrayyan72@gmail.com>
2024-11-10 22:28:42 +0500

    Add files via upload

---
+++ Milestone3/script.js
@@ -0,0 +1,39 @@
+"use strict";
+// Get references to the form and display area
+var form = document.getElementById('resume-form');
+var resumeDisplayElement = document.getElementById('resume-display');
+// Handle fomr submission
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // prevent page reload
+    // collect input values
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const education = document.getElementById('education').value;
+    const experience = document.getElementById('experience').value;
+    const skills = document.getElementById('skills').value;
+    //Generate the resume content dynamically
+    const resumeHTML = `
+    <h2><b>Resume</b></h2>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b>${name}</p>
+    <p><b>Email:</b>${email}</p>
+    <p><b>Phone:</b>${phone}</p>
+
+    <h3>Education</h3>
+    <p>${education}</P>
+
+    <h3>Experience</h3>
+    <p>${experience}</p>
+    
+    <h3>Skills</h3>
+    <p>${skills}</P>
+    `;
+    // Display the generated resume
+    if (resumeDisplayElement) {
+        resumeDisplayElement.innerHTML = resumeHTML;
+    }
+    else {
+        console.error(`the resume display element is missing.`);
+    }
+});
